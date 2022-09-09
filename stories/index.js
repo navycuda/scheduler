f3c696f44810b71be7e3c228f4a3a3cf1acb4099
NavyCuda <navysupra@gmail.com>
2022-09-08 20:41:00 -0700

    Add empty to stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,4 +146,7 @@ storiesOf("Appointment", module)
   />)
   .add("Header", () => <Header 
     time={'12pm'}
+  />)
+  .add("Empty", () => <Empty
+    onAdd={action('onAdd')}
   />);
