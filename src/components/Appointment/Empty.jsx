0b9fcd34ab21e7f63d70eb556f007e30d58edc04
NavyCuda <navysupra@gmail.com>
2022-09-08 20:34:57 -0700

    Add baseline of Empty

---
+++ src/components/Appointment/Empty.jsx
@@ -0,0 +1,16 @@
+// src/components/Appointment/Empty.jsx
+import React from 'react';
+
+const Empty = (props) => {
+  return (
+    <main className='appointment__add'>
+      <img
+        className='appointment__add-button'
+        src='images/add.png'
+        alt='add'
+      />
+    </main>
+  );
+};
+
+export default Empty;
