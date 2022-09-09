16c1cc8ed57c8e940d8a12584f0f397218347457
NavyCuda <navysupra@gmail.com>
2022-09-08 20:27:56 -0700

    Complete header build

--- src/components/Appointment/Header.jsx
+++ src/components/Appointment/Header.jsx
@@ -7,6 +7,8 @@ const Header = (props) => {
       <h4 className="text--semi-bold">
         {props.time}
       </h4>
+      <hr className="appointment__separator" />
     </header>
   );
 };
+export default Header;
