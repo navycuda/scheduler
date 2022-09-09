aa6ac9135fc60d5c7a4ef1e8737f847529b574e5
NavyCuda <navysupra@gmail.com>
2022-09-08 17:51:55 -0700

    Progress, I think

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,18 @@
+// src/components/InterviewerList.jsx
+import React from "react";
+
+const InterviewerList = (props) => {
+
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">
+        Interviewer
+      </h4>
+      <ul className="interviewers__list">
+
+      </ul>
+    </section>
+  );
+};
+export default InterviewerList;
