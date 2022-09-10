65c6b137b88be2c27aa4db58b85fb753498c5884
NavyCuda <navysupra@gmail.com>
2022-09-10 15:15:57 -0700

    Another miniscule bit of progress, confirm looks wrong

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,5 +153,11 @@ storiesOf("Appointment", module)
     onAdd={action('onAdd')}
   />)
   .add("Show", () => <Show 
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action('onEdit')}
+    onDelete={action('onDelete')}
+  />)
+  .add('Confirm', () => <Confirm
   
   />);
