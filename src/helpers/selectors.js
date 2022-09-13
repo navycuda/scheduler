4a37f8ac565535c8ac9493d91f61bcde29b77493
NavyCuda <navysupra@gmail.com>
2022-09-13 09:54:48 -0700

    Complete tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,16 @@
 // src/helpers/selectors.js
+export const getAppointmentsByIds = (appointments, ids) => {
+  return ids.map((id) => {
+    return appointments[id];
+  });
+};
+
 export const getAppointmentsForDay = (state, day) => {
-
-
-}
+  const selectedDay = state.days.find(({ name }) => name === day);
+  if (selectedDay) {
+    console.log('selectedDay: ', selectedDay);
+    console.log('day', day);
+    return getAppointmentsByIds(state.appointments, selectedDay.appointments);
+  }
+  return [];
+};
