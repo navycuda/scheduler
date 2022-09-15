ff3023d824283c979e8bedc08b1c5e14cc8a0175
NavyCuda <navysupra@gmail.com>
2022-09-14 20:12:53 -0700

    Add useVisualMode

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,6 +5,11 @@ import './styles.scss';
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+
 
 const Appointment = (props) => {
   return (
