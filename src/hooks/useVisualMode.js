74faec4842da53e71a246ab918102121ae348e51
NavyCuda <navysupra@gmail.com>
2022-09-14 17:49:25 -0700

    Get help to understand useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,22 @@ import { useState } from "react";
 
 export default function useVisualMode(initial){
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (destination) => {
+    //setMode(destination);
+    setHistory((current) => [...current, destination]);
+  };
+  const back = () => {
+    setHistory((previous) => {
+      if (previous.length < 2) {
+        return previous;
+      } else {
+        return previous.slice(0, previous.length - 1);
+        
+      }
+    });
+  }
+
+  return { mode:history[history.length - 1], transition, back };
 };
